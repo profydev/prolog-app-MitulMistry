0058dd119a88b873e714ee6d90d49087312c1aae
MitulMistry <MitulMistryDev@gmail.com>
2023-12-03 18:49:22 -0600

    Fix sidebar navigation arrow and flip it on collapse

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -91,6 +91,7 @@ export function SidebarNavigation() {
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
               className={styles.collapseMenuItem}
+              flipOnCollapse={true}
             />
           </ul>
         </nav>
