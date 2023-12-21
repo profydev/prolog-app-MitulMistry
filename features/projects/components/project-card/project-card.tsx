10c468a8fdca6edc2e0923c6913c93d58419aade
MitulMistry <MitulMistryDev@gmail.com>
2023-12-20 19:57:28 -0600

    Fix status badge colors in project cards

--- features/projects/components/project-card/project-card.tsx
+++ features/projects/components/project-card/project-card.tsx
@@ -17,9 +17,9 @@ const languageNames = {
 };
 
 const statusColors = {
-  [ProjectStatus.stable]: BadgeColor.success,
+  [ProjectStatus.info]: BadgeColor.success,
   [ProjectStatus.warning]: BadgeColor.warning,
-  [ProjectStatus.critical]: BadgeColor.error,
+  [ProjectStatus.error]: BadgeColor.error,
 };
 
 export function ProjectCard({ project }: ProjectCardProps) {
