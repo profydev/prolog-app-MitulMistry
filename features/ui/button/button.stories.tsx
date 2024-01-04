831bf407677d05afe3312375ca4039fe03952c0b
MitulMistry <MitulMistryDev@gmail.com>
2024-01-04 17:14:41 -0600

    Add button sizes

--- features/ui/button/button.stories.tsx
+++ features/ui/button/button.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Button } from "./button";
+import { Button, ButtonSize } from "./button";
 
 // Storybook CSF3 format
 
@@ -10,4 +10,20 @@ type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   args: { children: "Button CTA" },
+};
+
+export const Small: Story = {
+  args: { ...Default.args, size: ButtonSize.Small },
+};
+
+export const Medium: Story = {
+  args: { ...Default.args, size: ButtonSize.Medium },
+};
+
+export const Large: Story = {
+  args: { ...Default.args, size: ButtonSize.Large },
+};
+
+export const XLarge: Story = {
+  args: { ...Default.args, size: ButtonSize.XLarge },
 };
--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,6 +1,28 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./button.module.scss";
 import { UnstyledButton } from "./unstyled-button";
 
-export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <UnstyledButton {...props} />;
+export enum ButtonSize {
+  Small = "sm",
+  Medium = "md",
+  Large = "lg",
+  XLarge = "xl",
+}
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  size?: ButtonSize;
+};
+
+export function Button({
+  className,
+  size = ButtonSize.Medium,
+  ...props
+}: ButtonProps) {
+  return (
+    <UnstyledButton
+      className={classNames(styles.button, styles[size], className)}
+      {...props}
+    />
+  );
 }
