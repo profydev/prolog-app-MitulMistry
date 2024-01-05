b9a376b1178ef796542b28fa06b6546e3946f3e0
MitulMistry <MitulMistryDev@gmail.com>
2024-01-04 20:12:54 -0600

    Add remaining color and empty Button variants

--- features/ui/button/button.stories.tsx
+++ features/ui/button/button.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Button, ButtonSize, ButtonColor } from "./button";
+import { Button, ButtonSize, ButtonColor, ButtonVariant } from "./button";
 
 // Storybook CSF3 format
 
@@ -10,24 +10,86 @@ type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   args: { children: "Button CTA", disabled: false },
+  argTypes: {
+    color: {
+      control: "select",
+      options: Object.values(ButtonColor),
+    },
+  },
 };
 
 export const Small: Story = {
+  ...Default,
   args: { ...Default.args, size: ButtonSize.Small },
 };
 
 export const Medium: Story = {
+  ...Default,
   args: { ...Default.args, size: ButtonSize.Medium },
 };
 
 export const Large: Story = {
+  ...Default,
   args: { ...Default.args, size: ButtonSize.Large },
 };
 
 export const XLarge: Story = {
+  ...Default,
   args: { ...Default.args, size: ButtonSize.XLarge },
 };
 
 export const Primary: Story = {
+  ...Default,
   args: { ...Default.args, color: ButtonColor.Primary },
+};
+
+export const Secondary: Story = {
+  ...Default,
+  args: { ...Default.args, color: ButtonColor.Secondary },
+};
+
+export const Gray: Story = {
+  ...Default,
+  args: { ...Default.args, color: ButtonColor.Gray },
+};
+
+export const Error: Story = {
+  ...Default,
+  args: { ...Default.args, color: ButtonColor.Error },
+};
+
+export const PrimaryEmpty: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    color: ButtonColor.Primary,
+    variant: ButtonVariant.Empty,
+  },
+};
+
+export const SecondaryEmpty: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    color: ButtonColor.Secondary,
+    variant: ButtonVariant.Empty,
+  },
+};
+
+export const GrayEmpty: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    color: ButtonColor.Gray,
+    variant: ButtonVariant.Empty,
+  },
+};
+
+export const ErrorEmpty: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    color: ButtonColor.Error,
+    variant: ButtonVariant.Empty,
+  },
 };
