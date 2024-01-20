2ee67a38f338b08678eae0a093b4d6d3dd8ab05b
MitulMistry <MitulMistryDev@gmail.com>
2024-01-19 19:47:28 -0600

    Add basic styling to Select component

--- features/ui/select/select.tsx
+++ features/ui/select/select.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import classNames from "classnames";
-// import styles from "./select.module.scss";
+import styles from "./select.module.scss";
 
 type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
   label?: string;
 };
 
 export function Select({ className, label, ...props }: SelectProps) {
+  // const selectElement = (
+  //   <select className={classNames(className, styles.select)} {...props}>
+  //     {props.children}
+  //   </select>
+  // );
+
+  if (label)
+    return (
+      <div>
+        <label className={styles.label}>
+          {label}
+          {/* {selectElement} */}
+          <select className={classNames(className, styles.select)} {...props}>
+            {props.children}
+          </select>
+        </label>
+      </div>
+    );
+
+  // return { selectElement };
   return (
-    <div>
-      {label && <label>{label}</label>}
-      <select className={classNames(className)} {...props}>
-        {props.children}
-      </select>
-    </div>
+    <select className={classNames(className, styles.select)} {...props}>
+      {props.children}
+    </select>
   );
 }
