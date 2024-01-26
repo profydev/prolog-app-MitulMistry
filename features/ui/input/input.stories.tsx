95f7f566702890a03bd77c0c08c958ef35b10837
MitulMistry <MitulMistryDev@gmail.com>
2024-01-26 13:34:06 -0600

    Add support for icons to Input component

--- features/ui/input/input.stories.tsx
+++ features/ui/input/input.stories.tsx
@@ -41,10 +41,34 @@ export const Placeholder: Story = {
   },
 };
 
-export const Error: Story = {
+export const Icon: Story = {
   ...Default,
   args: {
     ...Placeholder.args,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+      >
+        <path
+          d="M18.3332 5C18.3332 4.08334 17.5832 3.33334 16.6665 3.33334H3.33317C2.4165 3.33334 1.6665 4.08334 1.6665 5M18.3332 5V15C18.3332 15.9167 17.5832 16.6667 16.6665 16.6667H3.33317C2.4165 16.6667 1.6665 15.9167 1.6665 15V5M18.3332 5L9.99984 10.8333L1.6665 5"
+          stroke="#667085"
+          strokeWidth="1.66667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    ),
+  },
+};
+
+export const Error: Story = {
+  ...Default,
+  args: {
+    ...Icon.args,
     errorMessage: "This is an error message.",
   },
 };
--- features/ui/input/input.tsx
+++ features/ui/input/input.tsx
@@ -7,6 +7,7 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
   hint?: string;
   errorMessage?: string;
+  icon?: React.ReactNode;
 };
 
 export function Input({
@@ -14,16 +15,57 @@ export function Input({
   className,
   hint,
   errorMessage,
+  icon,
   ...props
 }: InputProps) {
   return (
     <div className={classNames(styles.input, className)}>
       <label className={styles.label}>
         {children}
+        {icon && (
+          <span className={styles.icon} aria-hidden>
+            {icon}
+          </span>
+        )}
         <input
-          className={classNames(styles.inputBox, errorMessage && styles.error)}
+          className={classNames(
+            styles.inputBox,
+            icon && styles.iconOffset,
+            errorMessage && styles.error,
+          )}
           {...props}
         />
+        {errorMessage && (
+          <span className={styles.errorIcon} aria-hidden>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="17"
+              viewBox="0 0 16 17"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_17171_1729)">
+                <path
+                  d="M8.00016 5.45966V8.12632M8.00016 10.793H8.00683M14.6668 8.12632C14.6668 11.8082 11.6821 14.793 8.00016 14.793C4.31826 14.793 1.3335 11.8082 1.3335 8.12632C1.3335 4.44442 4.31826 1.45966 8.00016 1.45966C11.6821 1.45966 14.6668 4.44442 14.6668 8.12632Z"
+                  stroke="#F04438"
+                  strokeWidth="1.33333"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_17171_1729">
+                  <rect
+                    width="16"
+                    height="16"
+                    fill="white"
+                    transform="translate(0 0.126343)"
+                  />
+                </clipPath>
+              </defs>
+            </svg>
+          </span>
+        )}
       </label>
       {hint && !errorMessage && <span className={styles.hint}>{hint}</span>}
       {errorMessage && (
